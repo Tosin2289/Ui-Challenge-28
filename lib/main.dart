9c933fc98b0b645c4ab0eacdb062660c419253a6
Tosin2289 <ayoolatosin00@gmail.com>
2023-01-30 20:56:44 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,6 +26,66 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF726FF2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 100,
+              backgroundColor: Color(0xFF9289FC),
+              child: Stack(
+                children: [
+                  Transform.rotate(
+                    angle: -pi / 12,
+                    child: Image.asset("assets/image.png"),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Your personal\ntask manager",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                  fontSize: 23,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Text(
+                  "Free up a lot of time\nwith our app",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16, color: Colors.white.withOpacity(0.2)),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Color(0xFF9289FC),
+              child: CircleAvatar(
+                radius: 45,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
